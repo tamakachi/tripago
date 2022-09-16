52fb9203298ff2e5820910ab208cda9f9a76a743
Kent <kenttechnical@gmail.com>
2022-09-16 17:07:56 +0200

    useEffect hook for async fetch

---
+++ src/components/TripList.js
@@ -0,0 +1,31 @@
+
+import {useState, useEffect} from 'react'
+
+
+export default function TripList() {
+    const [trips,setTrips] = useState([]) 
+
+// To run a code block once and only once with useEffect, make the second argument array empty, this array is
+// known as the dependency array
+    useEffect(()=>{
+
+        fetch ("http://localhost:3000/trips")
+          .then(response=>response.json())
+          .then(json=>setTrips(json))
+          console.log(trips)
+        },[])
+
+  return (
+    <div>
+        <h2>Trip List</h2>
+        <ul>
+            {trips.map(trip => (
+                <li key={trip.id}>
+                    <h2>{trip.title}</h2>
+                    <h2>{trip.price}</h2>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
