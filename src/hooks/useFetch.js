8658a81f59c05ea6b4cdf955b52f80fd57f976a1
Kent <kenttechnical@gmail.com>
2022-09-20 17:05:24 +0200

    pending fetch state integration

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,10 +2,15 @@ import { useState, useEffect} from 'react'
 
 const useFetch = (url) => {
     const [data,setData] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+
     useEffect(()=>{
         const fetchData = async () =>{
+            setIsPending(true)
             const res = await fetch(url)
             const json = await res.json()
+            
+            setIsPending(false)
             setData(json)
         }
         fetchData()
