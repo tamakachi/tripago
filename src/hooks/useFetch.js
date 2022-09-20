e09fcf51d5bff71b0f0779cf25777845c331e81f
Kent <kenttechnical@gmail.com>
2022-09-20 16:29:04 +0200

    useFetch custom hook progress

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,5 +1,19 @@
-const useFetch = () => {
+import { useState, useEffect} from 'react'
 
+const useFetch = (url) => {
+    const [data,setData] = useState(null)
+    useEffect(()=>{
+        const fetchData = async () =>{
+            const res = await fetch(url)
+            const json = await res.json()
+            setData(json)
+        }
+    },[url])
+
+    fetchData()
+
+    return{data: data}
+    
 }
 
 export default useFetch
